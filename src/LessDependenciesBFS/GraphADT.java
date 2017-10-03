63326b91877873f9b99ed60af2181396d9b249fd
Tomas Chmelevskij 2015 <tomas.chmelevskij.2016@uni.strath.ac.uk>
2017-10-03 13:19:34 +0100

    Javadoc

--- src/LessDependenciesBFS/GraphADT.java
+++ src/LessDependenciesBFS/GraphADT.java
@@ -8,21 +8,24 @@ import java.util.ArrayList;
 public interface GraphADT {
     /**
      * Creates and adds new node to the graph.
-     * @param ID - id of the node.
+     *
+     * @param ID   - id of the node.
      * @param name - human readable string representation of the node.
      */
     void addNode(int ID, String name);
 
     /**
      * Edge connecting two nodes.
-     * @param inID - id of incoming connection.
-     * @param outID - id of outgoing direction.
+     *
+     * @param inID      - id of incoming connection.
+     * @param outID     - id of outgoing direction.
      * @param lineColor - color of the train line.
      */
     void addEdge(int inID, int outID, String lineColor);
 
     /**
      * Ids of nodes connected to given node.
+     *
      * @param n - id of the current node.
      * @return List of connected ids.
      */
@@ -30,8 +33,9 @@ public interface GraphADT {
 
     /**
      * Get just the path between start and end.
-     * @param id - source.
-     * @param id1 - destination.
+     *
+     * @param id   - source.
+     * @param id1  - destination.
      * @param path - all visited nodes.
      * @return path to take.
      */
