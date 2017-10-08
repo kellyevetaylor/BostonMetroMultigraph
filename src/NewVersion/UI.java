8e64e08e02f580398908ab24a0356c0b4dff826f
Dimitar Todorov CS2015 <dimitar.todorov.2015@uni.strath.ac.uk>
2017-10-08 17:26:10 +0100

    Small bug when you go from st.paul(B) to st.paul(C) and then from C to B. Otherwise seems to work.

--- src/NewVersion/UI.java
+++ src/NewVersion/UI.java
@@ -1,4 +1,4 @@
-package NewVersion;
+package NewVersion; 
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,6 +9,8 @@ public class UI {
 
     private String firstStationName;
     private String secondStationName;
+    private int firstStationId;
+    private int secondStationId;
     private static BostonMetroSystem bms;
     ArrayList<Integer> path = new ArrayList<Integer>();
 
@@ -23,13 +25,23 @@ public class UI {
     public String getSecondStationName(){
         return secondStationName;
     }
+    public int getFirstStationId(){
+        return firstStationId;
+    }
+
+    public int getSecondStationId(){
+        return secondStationId;
+    }
 
     public void selectStationName(BostonMetroSystem bms, boolean firstOrSecond) {
         String userInput;
+        String paul;
         Scanner sc = new Scanner(System.in);
+        List<INode> nodes = bms.getStations();
         while (1==1){
             if(firstOrSecond) {
                 System.out.println("Please type the name of your starting station or 'quit' to exit the program.");
+                
             } else {
                 System.out.println("Please type the name of your destination station or 'quit' to exit the program.");
             }
@@ -40,18 +52,70 @@ public class UI {
             if (userInput.equalsIgnoreCase("quit")){
                 quit();
             }
+   //NEW STUFF-----------------------------------------         
+        	if (userInput.equalsIgnoreCase("st.paulstreet")) {
+   		    System.out.println("If you mean the one on Green B, input B.");
+   		    System.out.println("If you mean the one on Green C, input C.");
+   		    paul = sc.next();
+   		    
+   		    if (paul.equalsIgnoreCase("b")) {
+   		    	if(firstOrSecond){
+   		    		firstStationId=38;
+   		    		firstStationName = "st.paulstreet";
+   		    	}
+   		    	else{
+   		    		secondStationId=38;
+   		    		secondStationName = "st.paulstreet";
+   		    	}
+   		    }
+   		   if (paul.equalsIgnoreCase("c")) {
+		    	if(firstOrSecond){
+		    		firstStationId=61;
+		    		firstStationName = "st.paulstreet";
+		    	}
+		    	else{
+		    		secondStationId=61;
+		    		secondStationName = "st.paulstreet";
+		    	}
+		    }
+        	}
+   		    	
+   		   
+        	
+
             if (doesStationExist(bms, userInput)){
+            	
                 if(firstOrSecond) {
+                	if (firstStationId!=31 && firstStationId!=68) {
                     firstStationName = userInput;
+                	}
                     break;
                 } else {
+                	if (secondStationId!=31 && secondStationId!=68)
                     secondStationName = userInput;
                     break;
                 }
-            } else {
+            } 
+            else {
                 System.out.println("Sorry, that name doesn't match any station. Please try again.");
             }
-        }
+            
+           
+            
+            
+            
+        	}
+        for(int i = 0; i < nodes.size(); i++) {
+    		if (nodes.get(i).getName().equalsIgnoreCase(firstStationName) && !nodes.get(i).getName().equalsIgnoreCase("st.paulstreet")) {
+    			firstStationId=nodes.get(i).getId();
+    		}
+    		if (nodes.get(i).getName().equalsIgnoreCase(secondStationName) && !nodes.get(i).getName().equalsIgnoreCase("st.paulstreet")) {
+    			secondStationId=nodes.get(i).getId();
+    		}
+    		 
+    }
+      //NEW STUFF--------------------------------
+
     }
 
     private boolean doesStationExist(BostonMetroSystem bms, String stationName) {
@@ -61,6 +125,7 @@ public class UI {
                 return true;
             }
         }
+        
         return false;
     }
 
